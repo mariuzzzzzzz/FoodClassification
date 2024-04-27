4ae0d18c272a1351b351e350e4c247110a5ac689
Marius Affolter <mariusaffolter@outlook.de>
2024-04-27 08:53:25 +0200

    changes food classification

---
+++ src/main/java/ch/zhaw/deeplearningjava/food/FoodApplication.java
@@ -0,0 +1,15 @@
+package ch.zhaw.deeplearningjava.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApplication.class, args);
+
+        System.out.println("Hello World");
+	}
+
+}
